10ce769fff4f60ed7be7acd026cf77f9957d424e
Chidi-26 <167634778+Chidi-26@users.noreply.github.com>
2024-05-13 18:18:31 +0100

    https:/<aws-0-eu-west-2.pooler.supabase.com>/rest/v1/<"People">?apikey=<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwcGJlZmdpdGZubGV4Ymx3ZGRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2MTAzMDIsImV4cCI6MjAzMTE4NjMwMn0.YAKUyD9tkyXXy4WD7L5y-D_-1pOnBDVdt1T8L9s3Jp4>

---
+++ SearchDate.js
@@ -0,0 +1,4 @@
+function peopleSearch(){
+    https:/<aws-0-eu-west-2.pooler.supabase.com>/rest/v1/<"People">?apikey=<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwcGJlZmdpdGZubGV4Ymx3ZGRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2MTAzMDIsImV4cCI6MjAzMTE4NjMwMn0.YAKUyD9tkyXXy4WD7L5y-D_-1pOnBDVdt1T8L9s3Jp4>
+
+}
